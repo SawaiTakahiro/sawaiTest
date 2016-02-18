869ec40c48302b926c12da04edbc87708dca3d6c
SawaiTakahiro <narcissis.jiteki+git@gmail.com>
2016-02-18 09:52:06 +0900

    ためしに表示を入れてみた

--- ruby/cui_window.rb
+++ ruby/cui_window.rb
@@ -7,6 +7,10 @@
  昔のドラクエみたいなウインドウっぽく表示してみたい。
 =end
 
+def show_window(data)
+	puts data
+end
+
 data = Hash.new
 data.store("HP", 100)
 data.store("MP", 100)
@@ -14,4 +18,19 @@ data.store("Attack", 100)
 data.store("Defense", 100)
 data.store("Speed", 100)
 
+show_window(data)
 
+
+#サンプル
+for i in 0..100 do
+	printf "\e[1A\e[1A\e[1A\e[1A\e[1A \r"	if i > 0# ← カーソルを上に
+	p i
+	text =  "+--------+--------+\n"
+	text += "| HP #{100 + i} | MP 200 |\n"
+	text += "| HP 300 | MP 400 |\n"
+	text += "+--------+--------+\n"
+	
+	sleep 0.1
+	
+	puts text
+end
