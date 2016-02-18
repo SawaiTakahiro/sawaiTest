c1e82e8030114c0e1673e40f6fb191be3eea0de1
SawaiTakahiro <narcissis.jiteki+git@gmail.com>
2016-02-18 09:39:47 +0900

    ファイルの追加

---
+++ ruby/cui_window.rb
@@ -0,0 +1,17 @@
+#! ruby -Ku
+
+=begin
+ 2016/02/18
+ 
+ 標準出力に整形して表示してみる
+ 昔のドラクエみたいなウインドウっぽく表示してみたい。
+=end
+
+data = Hash.new
+data.store("HP", 100)
+data.store("MP", 100)
+data.store("Attack", 100)
+data.store("Defense", 100)
+data.store("Speed", 100)
+
+
