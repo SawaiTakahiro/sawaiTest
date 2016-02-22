6fb893c8b83bc4a2b80d16f73fbdb22ceaa17a90
SawaiTakahiro <narcissis.jiteki+git@gmail.com>
2016-02-22 09:47:46 +0900

    なんかうまくいかない。あとで見直してみる

--- ruby/grid_window.rb
+++ ruby/grid_window.rb
@@ -10,17 +10,50 @@
 =end
 
 ##################################################
+def add_window(data, window, pos_x, pos_y)
+	x = pos_x
+	y = pos_y
+	
+	data.each_char do |char|
+		print "#{x}	#{y}	#{char}\n"
+		
+		#改行が入っていたら、次のところに
+		if char == "\n" then
+			x = pos_x
+			y += 1
+		else
+			#window[x][y] = char
+			window[x][y] = "1"
+			x += 1
+		end
+	end
+	
+	p window
+	
+	return window
+end
+
+##################################################
 
 #画面サイズ
-WIDTH		= 20
-HEIGHT	= 20
+WIDTH		= 10
+HEIGHT	= 10
 
 #画面の定義
 column = Array.new(WIDTH, "0")	#初期化用に作ってる。中身も仮で0にしてるけど、今度変える
 window = Array.new(HEIGHT, column)
 
+
+#ためしに書き込んでみるデータ
+hoge = <<"EOS"
++---+
+|999|
++---+
+EOS
+
+window = add_window(hoge, window, 2, 2)
+
 #表示してみる
 window.each do |row|
 	p row.join
 end
-
