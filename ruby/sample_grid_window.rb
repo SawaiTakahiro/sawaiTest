b43ae250189b32fbcbd92c76b3d62c60577b9581
SawaiTakahiro <narcissis.jiteki+git@gmail.com>
2016-02-25 09:48:49 +0900

    個々のブロック部分を整理してみるサンプル。
    罫線をこんな感じにして組み合わせるはずなんだよな…
    ヒアドキュメントで無理やり埋め込んでいるところを、こんな感じで生成できるようにすれば。
    
    ブロックの生成
    ↓
    それを指定座標に配置する
    みたいな

---
+++ ruby/sample_grid_window.rb
@@ -0,0 +1,13 @@
+#! ruby -Ku
+
+KEISEN_H = "-"
+KEISEN_V = "|"
+KEISEN_C = "+"
+
+len_status = 5
+
+
+print KEISEN_C, KEISEN_H * len_status, KEISEN_C, "\n"
+print KEISEN_V, "  999", KEISEN_V, "\n"
+print KEISEN_C, KEISEN_H * len_status, KEISEN_C, "\n"
+
