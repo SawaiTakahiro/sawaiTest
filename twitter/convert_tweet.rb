5f0ba7b42ef0c294188578481f90b442b16fd273
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-04-16 12:00:27 +0900

    とりあえずとりこめた。
    sqliteのファイルに、読み込んだcsvのデータを取り込めた。
    →そこから先はどうするか考える。あと、整理しないと…
    
    とはいえとりあえず動くからよし。

--- twitter/convert_tweet.rb
+++ twitter/convert_tweet.rb
@@ -12,6 +12,11 @@
 require "Twitter"
 require "./library.rb"
 
+require "sqlite3"
+include SQLite3
+
+TABLE_NAME = "fuga"
+
 #つぶやきを、見出しとアドレスに分けられたら分ける
 def text_split(text)
 	matchdata = text.match(/(.*.)(http.*.)/)
@@ -29,16 +34,25 @@ def text_split(text)
 	return [title, address]
 end
 
+#実際の処理
 list_tweet = read_csv("./output.csv")
-list_tweet.each do |tweet|
-	id = tweet[0]
-	date = tweet[1]
-	
-	#つぶやきの内容は、分けられたら分ける
-	data = text_split(tweet[2])
-	title = data[0]
-	address = data[1]
-	
-	p id, date, title, address
-	break
+
+db = SQLite3::Database.new("./arcive.db")	#ファイルなかったら作られるから平気
+db.execute("create table if not exists #{TABLE_NAME}(id primary key, date, title, address);")	#初期化用テーブル
+
+db.transaction do
+	list_tweet.each do |tweet|
+		id = tweet[0]
+		date = tweet[1]
+		
+		#つぶやきの内容は、分けられたら分ける
+		data = text_split(tweet[2])
+		title = data[0].gsub(/'|’/, "’")	#半角'を全角にエスケープしとく
+		#なんかブログ名とかで'とか`とか使われていると取り込めないので注意。とりあえず適当な文字に置き換えている
+		address = data[1]
+		
+		query = "insert or ignore into #{TABLE_NAME} values('#{id}', '#{date}', '#{title}', '#{address}')"
+		db.execute(query)
+	end
 end
+
